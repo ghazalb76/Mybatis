ff7d6026f320fb266b90c8675b531f0653221f1b
qhazzale <g.bakhtiari@tourismit.ir>
2020-12-22 00:03:44 +0330

    Add insert query

--- src/main/java/com/mybatis/demo/Controller.java
+++ src/main/java/com/mybatis/demo/Controller.java
@@ -1,6 +1,8 @@
 package com.mybatis.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -15,5 +17,10 @@ public class Controller {
     @GetMapping("/list")
     public List listUser() {
         return um.list();
+    }
+
+    @PostMapping("/insert")
+    public void insert(@RequestBody Test test) {
+         um.insert(test);
     }
 }
